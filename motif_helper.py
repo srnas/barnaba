06bf4cf76d8ba14122e6bbffa835e33ba806ff24
SandroBottaro <sbottaro@sissa.it>
2014-06-07 00:28:47 +0200

    motif helper v2

--- motif_helper.py
+++ motif_helper.py
@@ -12,7 +12,7 @@ for line in fh:
          lista = []
          for ll in line.split("-")[1:]:
              lista.extend(ll.split())
-         d = [line.split()[1],int(line.split()[2]),lista]
+         d = [line.split()[1],line.split()[0],lista]
          barnaba.append(d)
 fh.close()
 
@@ -24,12 +24,12 @@ for ii,el in enumerate(barnaba):
     pdb = open(el[0],'r')
     fh_out = open(pdb_out,'w')
     string = "REMARK " + el[0] + '\n'
+    string += "REMARK DISTANCE:" + str(el[1]) + "\n"
     string += "REMARK " + str(el[2]) + "\n"
-    print ""
     fh_out.write(string)
-
+    nr = 0
     for line in pdb:
-
+        
         if(line.split()[0] != "ATOM"):
             continue
         
@@ -38,7 +38,10 @@ for ii,el in enumerate(barnaba):
         REST = line[17:20].strip()
         RES_ID = RESN + "_" + REST + "_" + CHAIN
         if(RES_ID in el[2]):
+            nr += 1
             fh_out.write(line)
+    print "# Printing motif",str(el[2])
+    print "# output written to:",pdb_out, "(",nr,")"
 
     pdb.close()
     fh_out.close()
