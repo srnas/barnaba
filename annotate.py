8c14fdc0c444bb1e260eb461ee58ebcfeca8635e
SandroBottaro <sbottaro@sissa.it>
2014-07-09 15:19:55 +0200

    add file separator to ANNOTATE

--- annotate.py
+++ annotate.py
@@ -13,22 +13,26 @@ def annotate(args,files):
 
     for f in files:
         atoms,sequence = pb.get_coord(f)
+        count = 0
         for model,seq in zip(atoms,sequence):
+            count += 1
+            string = '# ' + f + "-" + str(count) + " "
             lcs,origo = t.coord2lcs(model)
             mat = t.lcs2mat_3d(lcs,origo,args.cutoff)
             int_mat = t.analyze_mat(mat,seq)
             if(args.compact==True):
-                string = ''
                 for j in range(int_mat.shape[0]):
                     for k in range(j+1,int_mat.shape[0]):
                         string += "%4s " % (t.interactions[int(int_mat[j,k])])
                 fh.write(string + "\n")
             else:
+                string += "\n"
+                fh.write(string)
                 for j in range(int_mat.shape[0]):
                     for k in range(j+1,int_mat.shape[0]):
                         if(int_mat[j,k] != 0):
-                            str = "%10s %10s %4s \n" % (seq[j],seq[k],t.interactions[int(int_mat[j,k])])
-                            fh.write(str)
+                            string  = "%10s %10s %4s \n" % (seq[j],seq[k],t.interactions[int(int_mat[j,k])])
+                            fh.write(string)
     fh.close()
     return 0
             
