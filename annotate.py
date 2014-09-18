b7633425919072509d0f0c4ccd2bc20470a84682
Sandro <sbottaro@sissa.it>
2014-09-18 11:51:38 +0200

    added dot-bracket notation

--- annotate.py
+++ annotate.py
@@ -33,6 +33,9 @@ def annotate(args,files):
             lcs,origo = t.coord2lcs(model)
             mat = t.lcs2mat_3d(lcs,origo,args.cutoff)
             int_mat = t.analyze_mat(mat,seq)
+            seq_string = ''
+            num_string = ''
+            ss_string = ['.']*len(seq)
             if(args.compact==True):
                 for j in range(int_mat.shape[0]):
                     for k in range(j+1,int_mat.shape[0]):
@@ -42,10 +45,20 @@ def annotate(args,files):
                 string += "\n"
                 fh.write(string)
                 for j in range(int_mat.shape[0]):
+                    seq_string += "%s" % seq[j].split("_")[1]
+                    num_string += "%s" % seq[j].split("_")[0]
                     for k in range(j+1,int_mat.shape[0]):
+                        if(t.interactions[int(int_mat[j,k])]=='WC'):
+                            ss_string[j] = "("
+                            ss_string[k] = ")"
+
                         if(int_mat[j,k] != 0):
                             string  = "%10s %10s %4s \n" % (seq[j],seq[k],t.interactions[int(int_mat[j,k])])
                             fh.write(string)
+                fh.write("# DOT-BRACKET ANNOTATION \n")
+                #fh.write("# " + num_string + "\n")
+                fh.write("# " + seq_string + "\n")
+                fh.write("# " + ''.join(ss_string) + "\n")
     fh.close()
     return 0
             
