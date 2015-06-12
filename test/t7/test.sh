6622fbc46520850c5f98d95a4fd1c5df5a835bd5
Sandro <sbottaro@sissa.it>
2015-06-12 16:40:34 +0200

    added flag to test7

--- test/t7/test.sh
+++ test/t7/test.sh
@@ -4,8 +4,8 @@ set -e
 
 # calculate TORSION and pucker angles.
 
-$BARNABA --name 7_EXAMPLE_TOR TORSION  -f $DATA/1S72.pdb
-$BARNABA --name 7_EXAMPLE_PUC TORSION  -f $DATA/1S72.pdb
+$BARNABA --name 7_EXAMPLE_TOR TORSION  -f $DATA/1S72.pdb --hread
+$BARNABA --name 7_EXAMPLE_PUC TORSION  -f $DATA/1S72.pdb --hread
 
 
 
