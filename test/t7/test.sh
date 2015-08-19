48bff3a805e48d54340e63e7f45737f749f9e556
Sandro <sbottaro@sissa.it>
2015-08-19 14:21:51 +0200

    add jcoupling test

--- test/t7/test.sh
+++ test/t7/test.sh
@@ -7,6 +7,9 @@ set -e
 $BARNABA --name 7_EXAMPLE_TOR TORSION  -f $DATA/1S72.pdb --pucker --hread
 $BARNABA --name 7_EXAMPLE_TOR TORSION  -f $DATA/1S72.pdb --backbone --hread
 
+# calculate j couplings 
+$BARNABA --name 7_EXAMPLE_TOR TORSION -f $DATA/samples.pdb --jcoupling --hread
+
 
 
 
