b7e8284a6cb71fd96bd520194421d895578974cc
SandroBottaro <sbottaro@sissa.it>
2016-02-16 17:37:20 +0100

    enm refs

--- test/t6/test.sh
+++ test/t6/test.sh
@@ -5,6 +5,9 @@ set -e
 # Calculate ENM as described in 
 # Pinamonti et al "Elastic Network Model for RNA: a comparative assessment with MD and SHAPE experiments" (2015) 
 
-$BARNABA --name 6_EXAMPLE_SBP ENM -f $DATA/sample1.pdb --type SBP
+$BARNABA  ENM --pdb $DATA/sample1.pdb --type SBP -o ENM_1
+$BARNABA  ENM --pdb $DATA/sample1.pdb --type AA --cutoff 0.7 -o ENM_2
 
-$BARNABA --name 6_EXAMPLE_AA ENM -f $DATA/sample1.pdb --type AA --cutoff 7.0
+#$BARNABA  ENM --pdb $DATA/sample1.pdb --type SBP -o ENM_2
+
+
