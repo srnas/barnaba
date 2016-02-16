e71328d23c46ef1ced15abbbe4a91aab7444aba0
SandroBottaro <sbottaro@sissa.it>
2016-02-16 15:02:44 +0100

    t2

--- test/t2/test.sh
+++ test/t2/test.sh
@@ -3,8 +3,9 @@
 set -e
 
 # Calculate ESCORE for all models in samples.pdb using pdb 1S72 as "statistical force field".
-# -f option accepts multiple files
 
-$BARNABA --name ESCORE_EXAMPLE_1 ESCORE --force-field $DATA/1S72.pdb -f $DATA/samples.pdb
+$BARNABA ESCORE --ff $DATA/1S72.pdb --top $DATA/sample1.pdb --trj $DATA/samples.xtc -o ESCORE_1
+
+$BARNABA ESCORE --ff $DATA/1S72.pdb --pdb $DATA/sample1.pdb -o ESCORE_2
 
 
