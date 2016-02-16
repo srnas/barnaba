0ad928f508a91b791cec9a93919e12276dd7ae44
SandroBottaro <sbottaro@sissa.it>
2016-02-16 15:03:09 +0100

    t3

--- test/t3/test.sh
+++ test/t3/test.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Search for GNRA tetraloop in 1S72 PDB file. -f option accepts multiple files and/or concatenated PDB
+# Search for GNRA tetraloop in 1S72 PDB file. --pdb option accepts multiple files and/or concatenated PDB
 # 1. The maximum ERMSD treshold value can be specified using the --treshold option. As a rule of thumb, 
 #    values between 0.5-1.0 should work in most cases.  
-# 2. The maximum number of allowed bulged bases (max 2) can be specified using the --bulge option 
+# 2. The maximum number of allowed bulged bases can be specified using the --bulge option 
 
-$BARNABA --name 3_EXAMPLE_1 SS_MOTIF --query $DATA/GNRA.pdb -f $DATA/1S72.pdb --bulge 1 --treshold 0.6
+$BARNABA  SS_MOTIF --query $DATA/GNRA.pdb --pdb $DATA/1S72.pdb --bulge 1 --treshold 0.6 --dump -o SSMOTIF
