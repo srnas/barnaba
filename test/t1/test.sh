64d1f29933e0f9b680fdd8b610fa1cea8283e652
SandroBottaro <sbottaro@sissa.it>
2016-02-16 10:46:27 +0100

    change test1 (checkd)

--- test/t1/test.sh
+++ test/t1/test.sh
@@ -3,13 +3,10 @@
 set -e
 
 # Calculate ERMSD distance between sample1.pdb and sample2.pdb. -f option accepts multiple files
-$BARNABA --name ERMSD_EXAMPLE_1 ERMSD --pdb $DATA/sample1.pdb -f $DATA/sample2.pdb
+$BARNABA ERMSD --ref $DATA/sample1.pdb --pdb $DATA/sample2.pdb --per-res -o ERMSD_1
 
-# Calculate ERMSD distance between sample1.pdb and models in samples.pdb
-$BARNABA --name ERMSD_EXAMPLE_2 ERMSD --pdb $DATA/sample1.pdb -f $DATA/samples.pdb
+# Calculate ERMSD distance between sample1.pdb and samples.xtc trajectory
+$BARNABA ERMSD --ref $DATA/sample1.pdb --trj $DATA/samples.xtc --top  $DATA/sample1.pdb  -o ERMSD_2
 
-# Calculate ERMSD distance between sample1.pdb and sample2.pdb. 
-# Additionally calculate per-residue ERMSD flucutations
-$BARNABA --name ERMSD_EXAMPLE_3 ERMSD --pdb $DATA/sample1.pdb -f $DATA/sample2.pdb --per-res
 
 
