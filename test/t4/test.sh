70590d20b1db86e0992028802a7cd80d41f26a24
SandroBottaro <sbottaro@sissa.it>
2016-02-16 15:10:26 +0100

    t4

--- test/t4/test.sh
+++ test/t4/test.sh
@@ -3,5 +3,5 @@
 set -e
 
 # Search for SARCIN motif in 1S72 PDB file. -f option accepts multiple files and/or concatenated PDB
-# the maximum number of allowed per-strand bulged bases (max 2) can be specified using the -bulge option 
-$BARNABA --name 4_EXAMPLE DS_MOTIF --query $DATA/SARCIN.pdb -f $DATA/1S72.pdb --l1 8 --l2 7 --bulge 1 --treshold 0.65
+# the maximum number of allowed per-strand inserted can be specified using the -bulge option 
+$BARNABA DS_MOTIF --query $DATA/SARCIN.pdb --pdb $DATA/1S72.pdb --l1 8 --l2 7 --bulge 1 --treshold 0.65 --dump -o DSMOTIF
