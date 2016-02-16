996a14b4ac9e41215edb99ad9d0de17240cd5747
SandroBottaro <sbottaro@sissa.it>
2016-02-16 10:49:08 +0100

    removed examples directory

--- examples/2_ESCORE.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Calculate ESCORE for all models in samples.pdb using pdb 1S72 as "force field".
-# -f option accepts multiple files
-../baRNAba --name ESCORE_EXAMPLE_1 ESCORE --force-field DATA/1S72.pdb -f DATA/samples.pdb
-
-
