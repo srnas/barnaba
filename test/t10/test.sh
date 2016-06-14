ea449229189534ddf9b3830d2081cff306a1fd6a
sbottaro <sbottaro@sissa.it>
2016-06-14 17:07:22 +0200

    crap

--- test/t10/test.sh
+++ test/t10/test.sh
@@ -3,10 +3,10 @@
 set -e
 
 # calculate backbone and pucker angles
-$BARNABA SMM --gvec w_repetition.gvec --ref ref.gvec --ncluster 100 -o plain
+$BARNABA SMM --gvec w_repetition.gvec --ref ref_0 --ncluster 100 -o plain
 awk '{print $1,$2,$3,$4,$5,$6,$7,$8}' plain.SMM.rna.smm.dat > plain_smm.dat
 
-$BARNABA SMM --gvec no_repetition.gvec --weight weights.dat --ref ref.gvec --ncluster 100 -o weight
+$BARNABA SMM --gvec no_repetition.gvec --weight weights.dat --ref ref_1 --ncluster 100 -o weight
 awk '{print $1,$2,$3,$4,$5,$6,$7,$8}' weight.SMM.rna.smm.dat > weight_smm.dat
 
 
