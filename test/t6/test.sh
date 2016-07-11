9143c04e879135188ea05fe702a2ceedb1c350de
giopina <giopina@sissa.it>
2016-07-11 16:50:10 +0200

    Reset test

--- test/t6/test.sh
+++ test/t6/test.sh
@@ -5,8 +5,8 @@ set -e
 # Calculate ENM as described in 
 # Pinamonti et al "Elastic Network Model for RNA: a comparative assessment with MD and SHAPE experiments" (2015) 
 
-$BARNABA  ENM --pdb $DATA/sample1.pdb --type SBP -o ENM_1
-$BARNABA  ENM --pdb $DATA/sample1.pdb --type AA --cutoff 0.7 -o ENM_2
+$BARNABA  ENM --pdb $DATA/sample1.pdb --type SBP -o ENM_1 --shape
+$BARNABA  ENM --pdb $DATA/sample1.pdb --type AA --cutoff 0.7 -o ENM_2 --shape
 
 #$BARNABA  ENM --pdb $DATA/sample1.pdb --type SBP -o ENM_2
 
