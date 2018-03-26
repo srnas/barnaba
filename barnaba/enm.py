142f49ab325cd15947d738977257314c0d49766a
giopina <giopina88@gmail.com>
2018-03-26 18:38:33 +0200

    add function to compute C2-C2 fluctuations between all pairs, as a matrix

--- barnaba/enm.py
+++ barnaba/enm.py
@@ -247,7 +247,84 @@ class Enm:
                 sigma.append(np.dot(diff,np.dot(tensor,diff)))
         return sigma, self.seq_c2
 
+    def get_c2_fluc_mat(self):
+        """ Computes the C2-C2 fluctuations matrix of an RNA ENM.
+        Return:
+        numpy array containing all pairwise C2-C2 fluctuations"""
+        # check if there are C2 atoms in the structure (needed to compute C2-C2 fluctuations...)
+        if(len(self.idx_c2)==0):
+            print("# no C2 atoms in PDB: can't compute C2-C2 fluctuations")
+            exit(1)
 
+        if self.sparse:
+            # To deal with big matrices the effective interaction matrix
+            # of the C2 beads is computed (see Pinamonti et al. NAR 2015)
+            ll=len(self.coords)
+            if len(self.idx_c2)*3==ll :
+                #skip effective interaction computation if there are only C2 beads
+                M_new=mat
+            else:
+                # submatrices
+                idx_a=np.sort(np.concatenate([3*self.idx_c2+i for i in range(3)]))
+                M_a=self.mat[np.ix_(idx_a,idx_a)]
+                idx_b=np.delete(np.arange(3*ll),idx_a)
+                M_b=self.mat[np.ix_(idx_b,idx_b)]
+                W=self.mat[np.ix_(idx_a,idx_b)]
+                
+                X=spsolve(M_b.tocsc(),W.T.tocsc())
+                ### X is not a sparse matrix... Damn...
+                # effective interaction matrix between C2 beads
+                M_new=(M_a-sp.csc_matrix.dot(W.tocsc(),X)).toarray()
+                eval_new,evec_new=eigh(M_new)
+            # Computing the fluctuations
+            fluc_mat=np.zeros((len(self.idx_c2),len(self.idx_c2)))
+            for n1 in range(len(self.idx_c2)):
+                for n2 in range(len(self.idx_c2)):
+                    i = 3*n1
+                    j = 3*n2
+                    diff = self.coords[self.idx_c2[n1]]-self.coords[self.idx_c2[n2]] 
+                    diff /= np.sqrt(np.sum(diff**2))
+                
+                    v_i = [evec_new[i:i+3,k] for k in range(6,len(eval_new))]
+                    v_j = [evec_new[j:j+3,k] for k in range(6,len(eval_new))]
+                
+                    top = len(eval_new)-6
+                
+                    c_ii = np.array([np.outer(v_i[k],v_i[k])/eval_new[k+6] for k in range(top)])
+                    c_jj = np.array([np.outer(v_j[k],v_j[k])/eval_new[k+6] for k in range(top)])
+                    c_ij = np.array([np.outer(v_i[k],v_j[k])/eval_new[k+6] for k in range(top)])
+                    c_ji = np.array([np.outer(v_j[k],v_i[k])/eval_new[k+6] for k in range(top)])
+                
+                    # sum contributions from all eigenvectors
+                    tensor = np.sum([(c_ii[k]+c_jj[k] - c_ij[k] - c_ji[k]) for k in range(top)],axis=0)
+                    fluc_mat[n1,n2]=fluc_mat[n2,n1]=np.dot(diff,np.dot(tensor,diff))
+
+        else:
+            fluc_mat=np.zeros((len(self.idx_c2),len(self.idx_c2)))
+            for n1 in range(len(self.idx_c2)):
+                for n2 in range(len(self.idx_c2)):
+                    i = 3*self.idx_c2[n1]
+                    j = 3*(self.idx_c2[n2])
+                    diff = self.coords[self.idx_c2[n1]]-self.coords[self.idx_c2[n2]] 
+                    diff /= np.sqrt(np.sum(diff**2))
+                
+                    v_i = [self.e_vec[i:i+3,k] for k in range(6,len(self.e_val))]
+                    v_j = [self.e_vec[j:j+3,k] for k in range(6,len(self.e_val))]
+                
+                    top = len(self.e_val)-6
+                
+                    c_ii = np.array([np.outer(v_i[k],v_i[k])/self.e_val[k+6] for k in range(top)])
+                    c_jj = np.array([np.outer(v_j[k],v_j[k])/self.e_val[k+6] for k in range(top)])
+                    c_ij = np.array([np.outer(v_i[k],v_j[k])/self.e_val[k+6] for k in range(top)])
+                    c_ji = np.array([np.outer(v_j[k],v_i[k])/self.e_val[k+6] for k in range(top)])
+            
+                    # sum contributions from all eigenvectors
+                    tensor = np.sum([(c_ii[k]+c_jj[k] - c_ij[k] - c_ji[k]) for k in range(top)],axis=0)
+                    fluc_mat[n1,n2]=fluc_mat[n2,n1]=np.dot(diff,np.dot(tensor,diff))
+            
+        return fluc_mat, self.seq_c2
+
+    
     def print_eval(self):
         """ Return a string containint the eigenvalues of the interaction matrix of the ENM"""
         stri = "# Eigenvalues \n"
